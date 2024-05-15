84bc5efd632d25fdf5d7bc10aad9d0da3462d406
Vaishnavigupta97 <vaishnavigupta6389@gmail.com>
2024-05-15 17:16:57 +0530

    change the error at compProduct file

--- src/Components/CompProducts.js
+++ src/Components/CompProducts.js
@@ -30,18 +30,29 @@ import Context from "../Store/Context";
 const CompProducts = (props) => {
     const {sideCartVisibleClick, setAllPurchaseCart, allPurchaseCart} = useContext(Context);
     const [movies, setMovies] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] =  useState(null);
     async function showProductsArr(){
-        const response = await fetch('https://swapi.dev/api/films');
-        const data = await response.json();
-        // then((data) => {
-            const transformedMovies = data.results.map(moviedata => {
-                return {
-                    id: moviedata.episode_id,
-                    title: moviedata.title
-                }
-            });
-            setMovies(transformedMovies);
-        // })
+        setIsLoading(true);
+        setError(null);
+        try{
+            const response = await  fetch('https://swapi.dev/api/films');
+            if(!response.ok){
+                throw new Error('Something went wrong ....Retrying');
+            }
+            const data = await response.json();
+                const transformedMovies = data.results.map(moviedata => {
+                    return {
+                        id: moviedata.episode_id,
+                        title: moviedata.title
+                    }
+                });
+                setMovies(transformedMovies);
+                setIsLoading(false);
+        }catch(error){
+            setError(error.message)
+        }
+        setIsLoading(false);
     }
     const addAtSideCard = (product) => {
         const productObj = {
@@ -57,7 +68,7 @@ const CompProducts = (props) => {
         <>
             <div className="d-flex flex-wrap justify-content-center" style={{ gap: "15%" }}>
                 <button onClick={showProductsArr}>fetch movies</button>
-                {movies.map((product, index) => (
+                {!isLoading && movies.map((product, index) => (
                         <Card className="w-40 my-5" key={index}>
                             <Card.Body className="border-none">
                                 <Card.Title className="text-center">{product.title}</Card.Title>
@@ -71,6 +82,10 @@ const CompProducts = (props) => {
                             </Card.Body>
                         </Card>
                 ))}
+                
+                {!isLoading && movies.length === 0 && !error &&<p>found no movies</p>}
+                {isLoading && <p>Loading...</p>}
+                {!isLoading && error && <p>{error}</p>}
             </div>
             <div className="m-auto" style={{width: "10%"}}>
                 <Button className="w-100 btn btn-secondary border-none fw-bolder fs-6" onClick={sideCartVisibleClick}>
