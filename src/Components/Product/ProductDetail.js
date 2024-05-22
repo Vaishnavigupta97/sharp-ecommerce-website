2dcc29ac6f676bbe8ace9a0dcbcdd08df6aa35a7
Vaishnavigupta97 <vaishnavigupta6389@gmail.com>
2024-05-22 16:38:51 +0530

    add images in product Inner

--- src/Components/Product/ProductDetail.js
+++ src/Components/Product/ProductDetail.js
@@ -1,10 +1,34 @@
 import {useParams} from "react-router-dom";
+import image1 from "../assets/black-tshirtInner.webp"
+import image2 from "../assets/black-jeans.webp";
+import classes from "./ProductDetail.module.css";
 
 const ProductDetail = () => {
     const params = useParams();
     console.log(params.productid);
+    if((params.productid) == 1){
+        return(
+            <>
+            <div className={classes.ProductDetailParent}>
+            <img src={image1} />
+            <p>this is blackInner t-shirt</p>
+            </div>
+            </>
+        )
+    }else{
+        return(
+            <>
+            <div className={classes.ProductDetailParent}>
+            <img src={image2} />
+            <p>this is black jeans</p>
+            </div>
+            </>
+        )
+    }  
     return(
         <>
+        
+        
         <h1>this is product detailssssssssssssssssss</h1>
         <p>{params.productid}</p>
         </>
--- src/Components/Product/ProductPage.js
+++ src/Components/Product/ProductPage.js
@@ -1,18 +1,51 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import image1 from '../assets/black-tshirt.webp';
+import image2 from '../assets/black-jeans.webp';
+import classes from "./ProductPage.module.css";
+import Compnavbar from "../CommonComp/Compnavbar";
+import CompHeader from "../CommonComp/CompHeader";
 
+// const data = [
+//     {
+//         id: 1,
+//         image : image1,
+//         title: "t-shirt",
+//         color: "black"
+//     },
+//     {
+//         id: 2,
+//         image : image2,
+//         title: "jeans",
+//         color: "black"
+//     }
+// ]
 const Productpage = () => {
-    return(
+    return (
         <>
-        <p>this is product page</p>
-        <ul>
-            <Link to='productpage/p1'> A book</Link>
-        </ul>
-        <ul>
-            <Link to='productpage/p2'> A carpet</Link>
-        </ul>
-        <ul>
-            <Link to='productpage/p3'> An online course</Link>
-        </ul>
+        <Compnavbar />
+        {/* <CompHeader /> */}
+            <div className={classes.productPageParent}>
+                {/* {
+                    data.map((dataDetail) => (
+                        <>
+                        <Link to></Link>
+                        <img src={dataDetail.image} />
+                        <p>{dataDetail.title}</p>
+                        <p>{dataDetail.color}</p>
+                        </>
+                    ))  
+                } */}
+                <ul>
+                    <Link to='productpage/1' className={classes.productImgParent}><img src={image1} /></Link>
+                    <Link to='productpage/2' className={classes.productImgParent}><img src={image2} /></Link>
+                    <Link to='productpage/1' className={classes.productImgParent}><img src={image1} /></Link>
+                    <Link to='productpage/2' className={classes.productImgParent}><img src={image2} /></Link>
+                    <Link to='productpage/1' className={classes.productImgParent}><img src={image1} /></Link>
+                    <Link to='productpage/2' className={classes.productImgParent}><img src={image2} /></Link>
+                    <Link to='productpage/1' className={classes.productImgParent}><img src={image1} /></Link>
+                    <Link to='productpage/2' className={classes.productImgParent}><img src={image2} /></Link>
+                </ul>
+            </div>
         </>
     )
 }
